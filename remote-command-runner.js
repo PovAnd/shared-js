814e8401bcd069e421aeebaa36222687a9eeb0e0
PovAnd <apovetko@gmail.com>
2025-04-03 18:53:21 +0200

    Упрощения в remote-command-runner.js

--- remote-command-runner.js
+++ remote-command-runner.js
@@ -2,8 +2,14 @@ $(function () {
     remoteAction();
 });
 
+
+
 function findContainerById(form, id) {
-    let container = $('#' + id);
+    let container = $(`#${id}`);
+    if (container.length === 0) {
+        $('<div>', {id}).appendTo('body');
+        container = $(`#${id}`);
+    }
     if (form) {
         let div = document.createElement("div");
         let formName = id + '-' + form.id;
@@ -15,32 +21,39 @@ function findContainerById(form, id) {
     }
 }
 
+function createJolokia(addr) {
+    let endpoint;
+    if (addr) {
+        endpoint = addr;
+    } else {
+        const juiIndex = window.juiIndex ?? '0';
+        endpoint = `https://${location.hostname}/jolokia/jui-${juiIndex}`;
+    }
+    return new Jolokia(endpoint);
+}
+
 function remoteAction(input) {
     let form = input ? input.parentElement : null;
     let status = findContainerById(form, 'status');
     const params = new URLSearchParams(location.search);
     const addr = params.get('addr');
     const routes = params.get('route') || params.get('routes');
-    const command = params.get('command');
-    const arg = params.get('arg') || '';
-    const display = params.get('display')
-    const raw = display === 'raw' || display === 'raw0';
-    const raw0 = display === 'raw0';
-    const juiIndexEl = document.getElementById('jui-index');
-    const juiIndex = juiIndexEl ? '0' : juiIndexEl.dataset.index;
 
     if (!addr && !routes) {
         status.text('Provide address or route!')
         return
     }
 
+    const command = params.get('command');
     if (!command) {
         status.text('Provide command!')
         return
     }
 
-    const endpoint = addr || `https://${location.hostname}/jolokia/jui-${juiIndex}`;
-    const j4p = new Jolokia(endpoint);
+    const arg = params.get('arg') || '';
+    const display = params.get('display')
+    const raw = display === 'raw' || display === 'raw0';
+    const raw0 = display === 'raw0';
 
     let out = findContainerById(form, 'output');
     if (form) {
@@ -49,11 +62,6 @@ function remoteAction(input) {
 
     if (raw0)
         out.addClass("raw0");
-
-    const errorHandler = function (res) {
-        status.text('ERROR');
-        out.append(document.createTextNode(res.stacktrace ? res.stacktrace : JSON.stringify(res)));
-    }
 
     let method = params.get('method');
     if (method) {
@@ -71,48 +79,51 @@ function remoteAction(input) {
         method = 'post';
     }
 
-    j4p.execute("com.forkshunter:type=RemoteCommandProcessor",
-        routes ? "requestExecuteMulti" : "requestExecute",
-        ...(routes ? [routes] : []),
-        command,
-        arg,
-        payload.size > 0 ? JSON.stringify(Object.fromEntries(payload)) : "",
-        {
-            success: function (id) {
-                pollExecutionResultViaJolokia(id, j4p, raw, status, out, errorHandler);
-            },
-            method: method,
-            error: errorHandler,
-            ajaxError: errorHandler
-        }
-    );
-}
-
-function pollExecutionResultViaJolokia(id, j4p, raw, status, out, errorHandler) {
-    const sleep = 3000;
-    let iterations = 1200000 / sleep;
-    const start = Date.now();
-
-    const refreshResult = function () {
-        j4p.execute("com.forkshunter:type=RemoteCommandProcessor", "executionResult", id, {
-            success: function (res) {
-                if (res.status === 'WAITING' && iterations-- > 0)
-                    setTimeout(refreshResult, sleep);
-
-                if (id === -1000) {
-                    status.text('Use scan report from cache');
-                } else {
-                    status.text(`${res.status} | ${((Date.now() - start) / 1000.0).toFixed(2)}s`);
-                }
-
-                for (const msg of res.messages) {
-                    out.append(raw ? msg : document.createTextNode(msg));
-                }
-            },
-            error: errorHandler,
-            ajaxError: errorHandler
-        })
+    const errorHandler = function (res) {
+        status.text('ERROR');
+        out.append(document.createTextNode(res.stacktrace ? res.stacktrace : JSON.stringify(res)));
     };
 
-    setTimeout(refreshResult, 1000);
+    const successHandler = function (mainRes) {
+        const sleep = 3000;
+        const start = Date.now();
+        let iterations = 1200000 / sleep;
+        const refreshResult = function () {
+            j4p.execute("com.forkshunter:type=RemoteCommandProcessor", "executionResult", mainRes, {
+                success: function (res) {
+                    if (res.status === 'WAITING' && iterations-- > 0)
+                        setTimeout(refreshResult, sleep);
+
+                    if (mainRes === -1000) {
+                        status.text('Use scan report from cache');
+                    } else {
+                        status.text(`${res.status} | ${((Date.now() - start) / 1000.0).toFixed(2)}s`);
+                    }
+
+                    for (const msg of res.messages) {
+                        out.append(raw ? msg : document.createTextNode(msg));
+                    }
+                },
+                error: errorHandler,
+                ajaxError: errorHandler
+            })
+        };
+
+        setTimeout(refreshResult, 1000);
+    };
+
+    createJolokia(addr)
+        .execute("com.forkshunter:type=RemoteCommandProcessor",
+            routes ? "requestExecuteMulti" : "requestExecute",
+            ...(routes ? [routes] : []),
+            command,
+            arg,
+            payload.size > 0 ? JSON.stringify(Object.fromEntries(payload)) : "",
+            {
+                method: method,
+                success: successHandler,
+                error: errorHandler,
+                ajaxError: errorHandler
+            }
+        );
 }
